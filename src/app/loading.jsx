8d9a05a154ed67a02ba722792ee6ec84d0f51e66
Eva-004 <foredajannateva@gmail.com>
2026-07-17 07:42:03 +0600

    Added loading spinner

---
+++ src/app/loading.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+
+const Loading = () => {
+  return (
+    <div>
+        <DotLottieReact
+      src="https://lottie.host/e3f53f84-a456-4b2f-9f18-2130a17a2f03/tyC74ZgQa0.lottie"
+      loop
+      autoplay
+    />
+    </div>
+  );
+};
+export default Loading;
